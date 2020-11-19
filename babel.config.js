ae7287993aa9b1cbfc146f6c32b20dc3d6ae8857
蒋从虎 <1239395043@qq.com>
2020-11-19 11:12:12 +0800

    引入ployfill

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  // plugins: [
+  //   [
+  //     "@babel/plugin-transform-runtime",
+  //     {
+  //       "absoluteRuntime": false,
+  //       "corejs": 3,
+  //       "version": "^7.12.5"
+  //     }
+  //   ]
+  // ]
 }
